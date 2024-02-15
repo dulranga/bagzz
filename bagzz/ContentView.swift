5b8e31b4cbb278ea747e26f33baa5c79fc4951d5
dulranga <dulranga.webdesign@gmail.com>
2024-02-15 21:51:48 +0530

    Initial Commit

---
+++ bagzz/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bagzz
+//
+//  Created by Dulranga Dhawanitha on 2024-02-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bagzz/bagzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  bagzzApp.swift
+//  bagzz
+//
+//  Created by Dulranga Dhawanitha on 2024-02-15.
+//
+
+import SwiftUI
+
+@main
+struct bagzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
